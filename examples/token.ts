65543bf9c8f56ea4f6afed14b862f8b2a396222f
Michael Beswick <michael.beswick@tesco.com>
2024-12-08 21:08:52 +0000

    Initial commit

---
+++ examples/token.ts
@@ -0,0 +1,134 @@
+import { createMachine } from '../src/createMachine';
+import { invariant } from '../src/lib/invariant';
+import { nanoid } from 'nanoid';
+
+async function mockFetchToken(credentials: {
+  username: string;
+  password: string;
+}) {
+  invariant(credentials.username, 'Invalid username');
+  invariant(credentials.password, 'Invalid password');
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return {
+    accessToken: nanoid(),
+    refreshToken: nanoid(),
+    expiresAt: Date.now() + 1000 * 60 * 60 * 24,
+  };
+}
+
+async function mockRefreshToken(refreshToken: string) {
+  invariant(refreshToken, 'Invalid refresh token');
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return {
+    accessToken: nanoid(),
+    refreshToken: nanoid(),
+    expiresAt: Date.now() + 1000 * 60 * 60 * 24,
+  };
+}
+
+type Events =
+  | { type: 'AUTHENTICATE'; data: { username: string; password: string } }
+  | { type: 'FETCH_RESPONSE' }
+  | { type: 'REFRESH_TOKEN' }
+  | { type: 'REFRESH_ERROR' };
+
+type Token = {
+  accessToken: string;
+  refreshToken: string;
+  expiresAt: number;
+};
+
+type Credentials = {
+  username: string;
+  password: string;
+};
+
+const machine = createMachine({
+  events: {} as Events,
+  context: {
+    token: null as Token | null,
+    attempts: 0,
+    credentials: null as Credentials | null,
+  },
+  initial: 'unauthenticated',
+  states: {
+    unauthenticated: {
+      initial: 'idle',
+      states: {
+        idle: {
+          on: {
+            AUTHENTICATE: ({ event, setContext }) => {
+              setContext('credentials', event.data);
+              return 'fetchingToken';
+            },
+          },
+        },
+        fetchingToken: {
+          onEntry: async ({ context, setContext }) => {
+            const { attempts, credentials } = context;
+
+            invariant(credentials, 'Invalid credentials');
+            setContext('attempts', attempts + 1);
+
+            try {
+              const token = await mockFetchToken(credentials);
+              setContext('token', token);
+
+              return 'authenticated';
+            } catch {
+              return 'fetchingTokenError';
+            }
+          },
+        },
+        fetchingTokenError: {},
+      },
+    },
+    authenticated: {
+      initial: 'waitForExpiration',
+      states: {
+        waitForExpiration: {
+          onEntry: ({ after, context }) => {
+            invariant(context.token, 'Invalid context');
+
+            after(context.token.expiresAt - Date.now(), () => {
+              return 'refreshingToken';
+            });
+          },
+        },
+        refreshingToken: {
+          onEntry: async ({ setContext, context }) => {
+            invariant(context.token, 'Invalid context');
+
+            try {
+              const { accessToken, refreshToken, expiresAt } =
+                await mockRefreshToken(context.token.refreshToken);
+
+              setContext('token', { accessToken, refreshToken, expiresAt });
+              setContext('attempts', 0);
+
+              return 'waitForExpiration';
+            } catch {
+              return 'refreshingTokenError';
+            }
+          },
+        },
+        refreshingTokenError: {
+          onEntry: ({ after }) => {
+            after(1000, () => {
+              return 'refreshingToken';
+            });
+          },
+        },
+      },
+    },
+  },
+});
+
+machine.subscribe((state) => {
+  console.log('Current state:', state);
+});
+
+machine.start();
---
+++ src/Machine.ts
@@ -0,0 +1,71 @@
+import { ValidateStateNode, StateNode } from './createMachine';
+import { MachineEvent, MachineNode } from './MachineNode';
+import { Subscription } from './Subscription';
+
+export class Machine<
+  E extends MachineEvent,
+  C extends object,
+  T extends StateNode<E, C>,
+> {
+  #rootNode: MachineNode<E, C>;
+  #subscription: Subscription;
+
+  constructor(config: ValidateStateNode<E, C, T> | MachineNode<E, C>) {
+    if (config instanceof MachineNode) {
+      this.#rootNode = config;
+    } else {
+      this.#rootNode = this.buildNodeTree(config as StateNode<E, C>);
+    }
+
+    this.#subscription = new Subscription();
+  }
+
+  get rootNode(): MachineNode<E, C> {
+    return this.#rootNode;
+  }
+
+  private buildNodeTree(
+    config: StateNode<E, C>,
+    id?: string,
+  ): MachineNode<E, C> {
+    const { states, ...rest } = config;
+    const rootNode = new MachineNode<E, C>({ ...rest, id });
+
+    if (states) {
+      Object.entries(states).forEach(([childId, childConfig]) => {
+        const isInitial = childId === config.initial;
+        const isFinial = childId == config.final;
+        const childNode = this.buildNodeTree(
+          childConfig as StateNode<E, C>,
+          childId,
+        );
+
+        rootNode.addChildState(childNode, isInitial, isFinial);
+      });
+    }
+
+    return rootNode;
+  }
+
+  start() {
+    this.#rootNode.enter();
+  }
+
+  stop() {
+    this.#rootNode.exit();
+  }
+
+  dispatch(event: E) {
+    this.#rootNode.dispatch(event);
+  }
+
+  subscribe(handler: (state: string) => void) {
+    return this.#subscription.subscribe(handler);
+  }
+
+  clone() {
+    const machine = new Machine<E, C, T>(this.#rootNode.clone());
+
+    return machine;
+  }
+}
